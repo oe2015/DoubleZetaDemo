ccaa6ac1791b016681353bf8db6e37be3f892bca
Ahmad1732002 <af3954@nyu.edu>
2024-03-07 17:54:25 +0400

    updated code

--- main.py
+++ main.py
@@ -83,13 +83,67 @@ def display_logo():
         st.image(logo_path1, use_column_width=True)  # This will center the image in the middle column
 
 def business_info_form():
+        # Inject custom CSS for styling the buttons to be larger and closer together
+    # Inject custom CSS for styling the buttons to fill the space
+    # Inject custom CSS for styling the buttons to fill the space
+    st.markdown("""
+    <style>
+    button {
+        display: block; /* Display block to fill the column width */
+        width: 100%; /* Set button width relative to the column width */
+        margin: 0.5rem 0; /* Standard margin for top and bottom */
+        padding: 0.75rem 1rem; /* Padding inside the buttons for a larger click area */
+        border-radius: 0.5rem; /* Rounded corners for buttons */
+    }
+    /* Style to reduce space around the button columns */
+    .col-padding {
+        padding-left: 2px; /* Adjust the left padding as needed */
+        padding-right: 2px; /* Adjust the right padding as needed */
+    }
+    </style>
+    """, unsafe_allow_html=True)
+
     st.subheader("Tell us about your business")
-    business_type = st.selectbox("What type of business are you?", ("Startup", "SME", "Freelancer"))
+
+    # Business Type Question
+    st.write("What type of business are you?")
+    business_type=st.session_state.get('business_type', None)
+    cols = st.columns(4)
+    business_types = [("🚀 Startup", "Startup"), ("🏢 SME", "SME"), 
+                    ("💼 Freelancer", "Freelancer"), ("📝 Other", "Other")]
+    for idx, (emoji, bt) in enumerate(business_types):
+        if cols[idx].button(f"{emoji}", key=f"business_{bt.lower()}"):
+            st.session_state['business_type'] = bt
+
+    # Check if 'Other' business type is specified and get input
+    if business_type == "Other":
+        st.session_state['business_type'] = st.text_input("Please specify your business type:")
+
     legal_form = st.selectbox("What is your legal form?", ("Company - Foreign Branch", "Company - Foreign GCC", "Company - GCC Branch", "Company - Local Branch", "Company - Non Local", "Establishment", "Establishment - Foreign", "Establishment - GCC", "Establishment - Local Branch", "Establishment - Mubdia'h", "Establishment - Non Local", "General Partnership", "Limited Liability Company", "Private Joint - Stock", "Professional Company", "Professional Establishment", "Public Joint - Stock", "Simple Limited Partnership", "Sole Proprietorship L.L.C.", "Sole Proprietorship PJSC"))
+  
+
+    # Office Location Question
     has_office = st.radio("Do you have an office in Abu Dhabi?", ("Yes", "No"))
-    sector = st.selectbox("What sector do you operate in?", ("Technology", "Healthcare", "Finance", "Education", "Other"))  # Update sectors as needed
+
+    # Sector Question with emoticons
+    st.write("What sector do you operate in?")
+
+    sector=st.session_state.get('sector', None)
+    cols = st.columns(5)
+    sectors = [("💻 Technology", "Technology"), ("🩺 Healthcare", "Healthcare"), 
+            ("💲 Finance", "Finance"), ("📚 Education", "Education"), ("📝 Other", "Other")]
+    for idx, (emoji, sec) in enumerate(sectors):
+        if cols[idx].button(f"{emoji}", key=f"sector_{sec.lower()}"):
+            st.session_state['sector'] = sec
+
+    # Check if 'Other' sector is specified and get input
+    if sector == "Other":
+        st.session_state['sector'] = st.text_input("Please specify your sector:")
+
+    # Business Description Question
     business_description = st.text_area("Give a short description about your business, your goals, and objectives")
-    
+
+    # Submit Button
     if st.button("Submit"):
         business_info = {
             "business_type": business_type,
@@ -98,9 +152,9 @@ def business_info_form():
             "sector": sector,
             "description": business_description
         }
-        update_user_info(st.session_state['username'], str(business_info))  # Consider a better serialization method or database structure
+        # Assume 'username' is obtained from Streamlit's session state or other context
+        update_user_info(st.session_state.get('username', 'default_username'), business_info)
         st.success("Business information saved!")
-
 
 def main():
     create_user_file()
@@ -114,18 +168,35 @@ def main():
 
     if st.session_state['logged_in']:
         st.title(f"Welcome to Nexus, {st.session_state['username']}!")
-        page = st.sidebar.selectbox("Menu", ["Update Information", "Profile", "Chatbot Interface"])
+        page = st.sidebar.selectbox("Menu", ["Chatbot Interface", "Profile","Update Information"])
+
         
         if page == "Update Information":
             # Allow users to update their business information
             st.subheader("Update Your Business Information")
-            business_info_form()
-
+            textForm = st.toggle("I prefer to write freely")
+            if textForm:
+                st.subheader("Tell us about your business")
+                business_description = st.text_area("Please provide a brief description of your business, including its core activities, industry, target market, and any key products or services offered",height=200)
+              
+            
+                if st.button("Submit"):
+                    
+                    business_info = {
+                        "business_description":business_description
+                    }
+                    update_user_info(st.session_state['username'], str(business_info))  # Consider a better serialization method or database structure
+                    st.success("Business information saved!")
+            else:
+                business_info_form()
+                
         elif page == "Profile":
             # Display user's existing information
             st.subheader("Your Profile")
             user_info = get_user_info(st.session_state['username'])
-            if user_info:
+            if user_info.get('business_description'):
+                st.write(f"**Business Description:** {user_info.get('business_description', 'Not specified')}")
+            elif user_info:
                 st.write(f"**Username:** {st.session_state['username']}")
                 st.write(f"**Business Type:** {user_info.get('business_type', 'Not specified')}")
                 st.write(f"**Legal Form:** {user_info.get('legal_form', 'Not specified')}")
@@ -139,12 +210,14 @@ def main():
             # st.subheader("Nexus AI Chatbot")
             user_info = get_user_info(st.session_state['username'])  # Assuming this returns business info
             # Format the context string using user_info
-            if user_info:
+            if user_info.get('business_description'):
+                context_str=(f"Please use the following information about the user when answering all his questions always address and talk to him using his/her name.The user you are talking to is {st.session_state['username']}. His business description is: {user_info['business_description']}")
+            elif user_info:
                 context_str = (
                     f"Please use the following information about the user when answering all his questions, always address and talk to him using his/her name. The user you are talking to is {st.session_state['username']}. His business type is: {user_info['business_type']}, his legal form is: {user_info['legal_form']}, he has an office?: {user_info['has_office']}, his business sector is: {user_info['sector']}, and his business description is: {user_info['description']}."
                 )
             else:
-                context_str = ""
+                context_str=""
             # Add a toggle for Private Mode in the sidebar
             # private_mode = st.checkbox('Private Mode', value=False)
             private_mode = st.toggle("Protect your data")
